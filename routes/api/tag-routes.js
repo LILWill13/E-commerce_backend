cd28c8aa6c46fd3fd9545ffb119902a97fd23435
noah gaston <noahsfuture18@gmail.com>
2022-05-10 08:54:17 -0400

    tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,4 +1,52 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
+router.get('/', (req, res) => {
+  try{
+    Tag.findAll({
+      include: [ { model: Product, through: ProductTag }]})
+    .then((tags) => res.status(200).json(tags));
+  } catch(err) {
+    res.status(500).json(err);
+  };
+});
+
+router.get('/:id', (req, res) => {
+  try{
+    Tag.findOne({ where: { id: req.params.id },
+      include: [{ model: Product, through: ProductTag }]})
+    .then((tag) => res.status(200).json(tag));
+  } catch(err) {
+    res.status(404).json(err);
+  };
+});
+
+router.post('/', (req, res) => {
+  try{
+    Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag));
+  } catch(err) {
+    res.status(404).json(err);
+  };
+});
+
+router.put('/:id', (req, res) => {
+  try{
+    Tag.update(req.body, { where: { id: req.params.id }})
+    .then((tag) => res.status(200).json(tag));
+  } catch(err) {
+    res.status(404).json(err);
+  };
+});
+
+router.delete('/:id', (req, res) => {
+  try{
+    Tag.destroy({ where: { id: req.params.id }})
+    .then((tag) => res.status(200).json(tag));
+  } catch(err) {
+    res.status(404).json(err);
+  };
+});
+
+
 module.exports = router;
