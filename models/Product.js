307ef30bdaa9fa4d86f804255e7306a241bd38a3
noah gaston <noahsfuture18@gmail.com>
2022-05-10 08:49:19 -0400

    models

--- models/Product.js
+++ models/Product.js
@@ -8,7 +8,41 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      reference: {
+        model: 'Category',
+        key: 'id',
+      }
+    },
+  },
+  { 
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
 );
 
 module.exports = Product;
